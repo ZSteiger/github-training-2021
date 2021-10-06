59125fe1461eff55baaf0f3b27acdc72da3538db
ZSteiger <zsteig93@gmail.com>
2021-10-06 11:37:01 -0400

    tests to good state

--- testing/main.go
+++ testing/main.go
@@ -1,4 +1,4 @@
-package testing
+package main
 
 import "fmt"
 
@@ -6,15 +6,22 @@ func main() {
 	// initialize a variable containing an int of 1
 	starterInt := 1
 
-	result := toTen(starterInt)
 	// pass to the function to be incremented
+	result := toTen(starterInt)
+
+	// print the result
 	fmt.Println(result)
 }
 
-func toTen(i int) int {
-	for i <= 10 {
-		fmt.Println(i)
-		i = i + 1
+func toTen(starterInt int) int {
+	// take in variable starterInt,
+	// if i is less than 10
+	for starterInt <= 10 {
+		// print its current value
+		fmt.Println(starterInt)
+		// increment it by one then reassign it the startInt variable
+		starterInt = starterInt + 1
 	}
-	return i
+	// return it from the function to be used in main()
+	return starterInt
 }
