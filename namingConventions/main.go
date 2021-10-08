15cdbe088af5d0cd67c242956f3eba124207e9e7
alex dripchak <alex.dripchak@eltoro.com>
2021-10-08 11:13:18 -0400

    added my changes

--- namingConventions/main.go
+++ namingConventions/main.go
@@ -4,66 +4,67 @@ import (
 	"fmt"
 	"math"
 	"sort"
-	"time"
 )
 
-type fancyShapeArithmetic interface {
-	insidebits() float64
-	outerbounds() float64
+type geometricCalculations interface {
+	getArea() float64
+	getPerimeter() float64
 }
 
-type multi_sided_quadrilateral struct {
-	theSides, howTallAmI float64
+type Rectangle struct {
+	width, height float64
 }
 
-type idk struct {
-	halfThe_diameter float64
+type Circle struct {
+	radius float64
 }
 
-func (instantiated_multi_sided_quadrilateral multi_sided_quadrilateral) insidebits() float64 {
-	return instantiated_multi_sided_quadrilateral.theSides * instantiated_multi_sided_quadrilateral.howTallAmI
+func (rect Rectangle) getArea() float64 {
+	return rect.width * rect.height
 }
 
-func (shape multi_sided_quadrilateral) outerbounds() float64 {
-	return 2*shape.theSides + 2*shape.howTallAmI
+func (shape Rectangle) getPerimeter() float64 {
+	return 2*shape.width + 2*shape.height
 }
 
-func (fuckCircles idk) insidebits() float64 {
-	return math.Pi * fuckCircles.halfThe_diameter * fuckCircles.halfThe_diameter
+func (circ Circle) getArea() float64 {
+	return math.Pi * circ.radius * circ.radius
 }
 
-func (what idk) outerbounds() float64 {
-	return 2 * math.Pi * what.halfThe_diameter
+func (circ Circle) getPerimeter() float64 {
+	return 2 * math.Pi * circ.radius
 }
 
-func takeTheInternalMeasurementsOfTheItemInQuestionAndReportBackTheResultOfTheBasicOperandsNecessaryForDeterminingTheCode(asdfghjkl fancyShapeArithmetic) {
-	switch time.Now().Weekday() {
-	case time.Friday:
-		fmt.Println("Today is the day 😎")
-	default:
-		fmt.Println("Keep dreaming 😭")
-	}
+func printAreaAndPerimeter(shape geometricCalculations) {
+	// TODO: Delete commented code below b/c why 
 
-	fmt.Printf("Result: %v", asdfghjkl)
-	fmt.Printf("Area: %v", asdfghjkl.insidebits())
-	fmt.Printf("Perimeter: %v", asdfghjkl.outerbounds())
+	// switch time.Now().Weekday() {
+	// case time.Friday:
+	// 	fmt.Println("Today is the day 😎")
+	// default:
+	// 	fmt.Println("Keep dreaming 😭")
+	// }
+
+	fmt.Printf("Result: %v, ", shape)
+	fmt.Printf("Area: %v, ", shape.getArea())
+	fmt.Printf("Perimeter: %v \n", shape.getPerimeter())
 }
 
-func sundayChores(hamper []string) {
-	sort.Strings(hamper)
-	fmt.Println(hamper)
+func printSortedStrings(array []string) {
+	sort.Strings(array)
+	fmt.Println(array)
 }
 
 // Go programs require package main, and func main to be declared as an entry point
 // no need to rename them
 func main() {
 
-	msq := multi_sided_quadrilateral{theSides: 3, howTallAmI: 4}
-	istilldontknow := idk{halfThe_diameter: 5}
+	rect := Rectangle{width: 3, height: 4}
+	circ := Circle{radius: 5}
 
-	takeTheInternalMeasurementsOfTheItemInQuestionAndReportBackTheResultOfTheBasicOperandsNecessaryForDeterminingTheCode(msq)
-	takeTheInternalMeasurementsOfTheItemInQuestionAndReportBackTheResultOfTheBasicOperandsNecessaryForDeterminingTheCode(istilldontknow)
+	printAreaAndPerimeter(rect)
+	printAreaAndPerimeter(circ)
 
 	dirty_laundry := []string{"underwear", "socks", "old tshirts", "work shorts", "facemask"}
-	sundayChores(dirty_laundry)
+	printSortedStrings(dirty_laundry)
 }
